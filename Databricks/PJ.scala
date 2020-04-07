cb70cb43892e21041846f7b374c037bdfa1f78a7
Tanguy-Pellerin <50319760+Tanguy-Pellerin@users.noreply.github.com>
2020-04-07 19:10:52 +0200

    update Databricks

--- Databricks/PJ.scala
+++ Databricks/PJ.scala
@@ -1,53 +1,63 @@
 // Databricks notebook source
 dbutils.fs.unmount("/mnt/s3Data")
 
-val AccessKey = "Place the AccessKey from the IAM user"
+val AccessKey = "X"
 // Encode the Secret Key as that can contain "/"
-val SecretKey = "Place the SecretKey from the IAM user"
+val SecretKey = "X"
 val EncodedSecretKey = SecretKey.replace("/", "%2F")
-val AwsBucketName = "alertdronebucket"
+val AwsBucketName = "X"
 val MountName = "s3Data"
 
 dbutils.fs.mount(s"s3a://$AccessKey:$EncodedSecretKey@$AwsBucketName", s"/mnt/$MountName")
 
 // COMMAND ----------
 
-display(dbutils.fs.ls(s"/mnt/$MountName/2020/04/04/"))
+display(dbutils.fs.ls(s"/mnt/$MountName/2020/04/"))
 
 // COMMAND ----------
 
-val file = spark.read.text("dbfs:/mnt/s3Data/2020/04/04/22/KinesisToS3-1-2020-04-04-22-17-32-9d9545c8-13c6-497f-8409-f1b18ca731cd")
-file.show()
+import java.util.Calendar
+
+val date = Calendar.getInstance.getTime.toString().split(" ")
+val day = date(2)
+val date4 = date(3).split(":")
+val hour = date4(0)
 
 // COMMAND ----------
 
 import org.apache.spark.sql.functions._
 import spark.implicits._
 
-val names = file.select("value").collectAsList()
-var test = names.toString()
+val all_files : Seq[String] = dbutils.fs.ls(s"/mnt/$MountName/2020/04/"+day+"/"+hour+"/").map(_.path)
+val file = spark.read.text(all_files(0))
 
-test = test.replace("[[", "[")
-test = test.replace("]]", "]")
-test = test.replace("}{", "},{")
-test = test.replace("} {", "},{")
-  
-val df = spark.read.json(Seq(test).toDS)
+val json = file.select("value").collectAsList().toString().replace("[[", "[").replace("]]", "]").replace("}{", "},{").replace("} {", "},{")
+ 
+val df = spark.read.json(Seq(json).toDS)
 
 // COMMAND ----------
 
-df.write.format("com.databricks.spark.csv").option("header","true").save("dbfs:/ooo.csv")
+df.groupBy("IdDrone").count().show()
+df.groupBy("IdDrone").agg(avg("DroneAltitude")).show()
+df.groupBy("IdDrone").agg(avg("Temperature"), avg("Speed")).show()
+df.groupBy("IdDrone").agg(avg("Speed")).show()
+df.groupBy("IdDrone").agg(avg("Temperature")).show()
+df.groupBy("IdDrone").agg(avg("DroneAltitude"), avg("Speed")).show()
 
 // COMMAND ----------
+
+val all_files : Seq[String] = dbutils.fs.ls(s"/mnt/$MountName/2020/04/"+day+"/"+hour+"/").map(_.path)
+val file = spark.read.text(all_files(0))
+
+val json = file.select("value").collectAsList().toString().replace("[[", "[").replace("]]", "]").replace("}{", "},{").replace("} {", "},{")
+ 
+val df = spark.read.json(Seq(json).toDS)
+df.write.format("com.databricks.spark.csv").option("header","true").save("dbfs:/final.csv")
 
 val sparkDF = spark.read.format("csv")
 .option("header", "true")
 .option("inferSchema", "true")
-.load("/ooo.csv")
-
-// COMMAND ----------
-
-display(sparkDF)
+.load("/final.csv")
 
 // COMMAND ----------
 
---
+++ Databricks/readme.txt
@@ -0,0 +1,2 @@
+Nous n'avons pas réussi à faire la dataviz en respectant le fait de garder l'instance de spark.
+Mais nous avons réussi à faire des statistiques avec.
